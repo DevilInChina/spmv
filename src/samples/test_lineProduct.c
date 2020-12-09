bff04aada677ecdc0b38893eb945799ee5cbc647
DevilInChina <905320627@qq.com>
2020-12-09 11:54:12 +0800

    rename

--- src/samples/test_lineProduct.c
+++ src/samples/test_lineProduct.c
@@ -6,89 +6,49 @@
 #include <stdio.h>
 #include <string.h>
 #define ALIEN 32
-
-void Test_s_LineProduct(const char*funcName,void (* LineProducts)
-        (const float*Val,const BASIC_INT_TYPE* indx,
-         const float *Vector_X,float *Vector_Y),int len,int iter){
-    int lenC = len;
-    //len<<=1;
-    float *X = aligned_alloc(ALIEN,sizeof(float )*len);
-    int *indx = aligned_alloc(ALIEN,sizeof(int )*len);
-    float *Val = aligned_alloc(ALIEN,sizeof(float )*len);
-    float *Y_golden = aligned_alloc(ALIEN,sizeof(float )*len);
-    float *Y = aligned_alloc(ALIEN,sizeof(float )*len);
-    for(int i = 0 ; i < lenC ; ++i){
-        indx[i] = lenC-1-i;
-        X[i] = i+1;
-        Val[i]=i+1;
-    }
-    for(int i = 0 ; i < lenC ; ++i){
-        Y_golden[i] = Val[i]*X[indx[i]];
-    }
-    for(int i = 0 ; i < iter ; ++i){
-        memset(Y,0,sizeof(float )*lenC);
-        LineProducts(Val,indx,X,Y);
-    }
-    int cnt = 0;
-    for(int i = 0 ; i < lenC ; ++i){
-        if(Y[i]!=Y_golden[i]){
-            ++cnt;
-            printf("%f %f\n",Y[i],Y_golden[i]);
-        }
-    }
-    printf("%s_errcnt=  %d in %d\n",funcName,cnt,lenC);
-    free(Y_golden);
-    free(Val);
-    free(Y);
-    free(indx);
-    free(X);
-
+#define Test_LineProduct(type)\
+void Test_##type##_LineProduct(const char*funcName,void (* LineProducts)\
+    (const type*Val,const BASIC_INT_TYPE* indx,\
+    const type *Vector_X,type *Vector_Y),int len,int iter) {\
+    int lenC = len;           \
+    type *X = aligned_alloc(ALIEN, sizeof(type) * len);\
+    int *indx = aligned_alloc(ALIEN, sizeof(int) * len);\
+    type *Val = aligned_alloc(ALIEN, sizeof(type) * len);\
+    type *Y_golden = aligned_alloc(ALIEN, sizeof(type) * len);\
+    type *Y = aligned_alloc(ALIEN, sizeof(type) * len);\
+    for (int i = 0; i < lenC; ++i) {\
+        indx[i] = lenC - 1 - i;\
+        X[i] = i + 1;\
+        Val[i] = i + 1;\
+    }\
+    for (int i = 0; i < lenC; ++i) { \
+        Y_golden[i] = Val[i] * X[indx[i]];\
+    }\
+    for (int i = 0; i < iter; ++i) {\
+        memset(Y, 0, sizeof(type) * lenC);\
+        LineProducts(Val, indx, X, Y);\
+    }\
+    int cnt = 0;\
+    for (int i = 0; i < lenC; ++i) {\
+        if (Y[i] != Y_golden[i]) {\
+            ++cnt;\
+            printf("%f %f\n", Y[i], Y_golden[i]);\
+        }\
+    }\
+    printf("%s_errcnt=  %d in %d\n", funcName, cnt, lenC);\
+    free(Y_golden);\
+    free(Val);\
+    free(Y);\
+    free(indx);\
+    free(X);                      \
 }
-void Test_d_LineProduct(const char*funcName,void (* LineProducts)
-        (const double*Val,const BASIC_INT_TYPE* indx,
-         const double *Vector_X,double *Vector_Y),int len,int iter){
 
-    int lenC = len;
-    //len<<=1;
-
-    double *X = aligned_alloc(ALIEN,sizeof(double )*len);
-
-    int *indx = aligned_alloc(ALIEN,sizeof(int )*len);
-
-    double *Val = aligned_alloc(ALIEN,sizeof(double )*len);
-    double *Y_golden = aligned_alloc(ALIEN,sizeof(double )*len);
-    double *Y = aligned_alloc(ALIEN,sizeof(double )*len);
-    for(int i = 0 ; i < lenC ; ++i){
-        indx[i] = lenC-1-i;
-        X[i] = i+1;
-        Val[i]=i+1;
-    }
-    for(int i = 0 ; i < lenC ; ++i){
-        Y_golden[i] = Val[i]*X[indx[i]];
-    }
-    for(int i = 0 ; i < iter ; ++i){
-        memset(Y,0,sizeof(double )*lenC);
-        LineProducts(Val,indx,X,Y);
-    }
-    int cnt = 0;
-    for(int i = 0 ; i < lenC ; ++i){
-        if(Y[i]!=Y_golden[i]){
-            ++cnt;
-            printf("%f %f\n",Y[i],Y_golden[i]);
-        }
-    }
-    printf("%s_errcnt=  %d in %d\n",funcName,cnt,lenC);
-    free(Y_golden);
-    free(Val);
-    free(Y);
-    free(indx);
-    free(X);
-
-}
+Test_LineProduct(double );
+Test_LineProduct(float );
 
 int main(){
     for(int i = 0 ; i < 9 ; ++i){
-        Test_d_LineProduct(Line_d_Products_name[i],Line_d_Products[i],4<<(i/3),1 );
-        Test_s_LineProduct(Line_s_Products_name[i],Line_s_Products[i],4<<(i/3),1 );
+        Test_double_LineProduct(Line_d_Products_name[i],Line_d_Products[i],4<<(i/3),1 );
+        Test_float_LineProduct(Line_s_Products_name[i],Line_s_Products[i],4<<(i/3),1 );
     }
 }
