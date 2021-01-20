a5f2eb0875ba79ede4a8ff0f64e7910a4765950d
DevilInChina <905320627@qq.com>
2021-01-20 21:27:34 +0800

    balanced 2 debuging

--- src/src_spmv/parallel_balanced2_spmv.c
+++ src/src_spmv/parallel_balanced2_spmv.c
@@ -171,8 +171,6 @@ void spmv_parallel_balanced2_Selected(
         }
     }
 
-    void *Ysum = malloc(size * nthreads);
-    void *Ypartialsum = malloc(size * nthreads);
 #pragma omp parallel for
     for (int tid = 0; tid < nthreads; tid++) {
         if (Yid[tid] == -1) {
@@ -190,19 +188,12 @@ void spmv_parallel_balanced2_Selected(
             }
         }
         if (Yid[tid] != -1 && Apinter[tid] <= 1) {
-            CONVERT_EQU(Ysum+tid*size,size,0);
             dotProductFunction(End2[tid] - Start2[tid],
                                ColIdx + Start2[tid], Matrix_Val + Start2[tid]*size, Vector_Val_X,
-                               Ypartialsum+tid*size,way
+                               Vector_Val_Y+Yid[tid]*size,way
                                );
-
-            CONVERT_ADDEQU(Ysum+tid*size,size,Ypartialsum+tid*size);
-
-            CONVERT_ADDEQU(Vector_Val_Y+Yid[tid]*size,size,Ysum+tid*size);
         }
     }
-    free(Ysum);
-    free(Ypartialsum);
 }
 
 
