ea1b8c99f8b27c1aaa548667596fb14c835151ab
DevilInChina <905320627@qq.com>
2021-02-05 03:09:09 +0800

    mkl bench debug

--- src/samples/mkl_spmv.c
+++ src/samples/mkl_spmv.c
@@ -85,11 +85,11 @@ void testForFunctions(const char *matrixName,
     //qsort(Y_golden,m,sizeof(VALUE_TYPE),cmp_s);
     for (BASIC_SIZE_TYPE thread = threads_begin; thread <= threads_end; thread <<= 1u) {
 
-        mkl_set_num_threads(thread);
+        //mkl_set_num_threads(thread);
         omp_set_num_threads(thread);
         gettimeofday(&t1, NULL);
         mkl_sparse_set_mv_hint(mat,
-                               SPARSE_OPERATION_NON_TRANSPOSE,descr,1000000
+                               SPARSE_OPERATION_NON_TRANSPOSE,descr,5000
         );
 
         gettimeofday(&t2, NULL);
